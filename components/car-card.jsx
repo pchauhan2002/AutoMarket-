d510559a88fb9efbcc42a9687e629fd91ac4a4c3
pchauhan2002 <pratosh01chauhan@gmail.com>
2025-06-19 15:44:18 +0530

    initial commit

---
+++ components/car-card.jsx
@@ -0,0 +1,82 @@
+"use client";
+import React, { useState } from 'react';
+import { Card, CardContent } from './ui/card';
+import Image from 'next/image';
+import { Button } from './ui/button';
+import { Heart } from 'lucide-react';
+import { Badge } from './ui/badge';
+import { useRouter } from 'next/navigation';
+const CarCard = ({car}) => {
+    const[isSaved,setSaved]=useState(car.wishlisted);
+    const handleToggleSave=async(e)=>{};
+    const router=useRouter()
+  return (
+  <Card className="overflow-hidden hover:shadow-lg transition group">
+    <div className="relative h-48">
+        {car.images && car.images.length>0 ? (
+            <div className="relative w-full h-full">
+  <Image 
+    src={car.images[0]} 
+    alt={`${car.make} ${car.model}`} 
+    fill
+    className="object-cover group-hover:scale-105 transition duration-300"
+  />
+</div>
+        ):(
+            <div></div>
+        )}
+    </div>
+    <div>
+    <Button 
+        variant="ghost" 
+        size="icon" 
+        className={`${
+             isSaved
+                ? "text-red-500 hover:text-red-600"
+                :"text-gray-600 hover:text-gray-900"
+            }`}
+        onClick={handleToggleSave}
+    >
+        <Heart className={isSaved ? "fill-current" :""} size={20}/>   
+    </Button>
+    </div>
+    <CardContent className="p-4">
+        <div>
+            <h3>{car.make}{car.model}</h3>
+            <span className="text-x1 font-boold text-blue-600">
+                ${car.price.toLocaleString()}
+            </span>
+        </div>
+
+        <div className='text gray-600 mb-2 flex items-center'>
+            <span>{car.year}</span>
+            <span className="mx-2">.</span>
+            <span>{car.transmission}</span>
+            <span className="mx-2"></span>
+            <span>{car.fuelType}</span>
+        </div>
+        <div className="flex wrap gap-1 mb-4">
+            <Badge variant="outline" className="bg-gray-50">
+                {car.bodyType}
+            </Badge>
+            <Badge variant="outline" className="bg-gray-50">
+                {car.mileage.toLocaleString()} miles
+            </Badge>
+            <Badge variant="outline" className="bg-gray-50">
+                {car.color}
+            </Badge>
+            </div>
+            <div className='flex justify-between'>
+                <Button 
+                    className=""
+                    onClick={()=> router.push(`/cars/{${car.id}}`)}
+                >
+                    View Car
+                </Button>
+            </div>
+    </CardContent>
+  </Card>
+  );
+};
+
+export default CarCard;
